adc191cb195e2815ef5a971599679c151c1ca2e6
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-07-25 09:16:12 +0200

    Add a tool to fill in gaps in a word with the available letters

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod permute;
 mod dictionary;
+mod word_solver;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/word_solver.rs
@@ -0,0 +1,64 @@
+// Waffle Solve
+// Copyright (C) 2023  Neil Roberts
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use super::dictionary::Dictionary;
+use super::permute;
+
+pub struct Iter<'a> {
+    dictionary: &'a Dictionary,
+    permuter: permute::Iter<'a, char>,
+    template: &'a str,
+    result_buf: String,
+}
+
+impl<'a> Iter<'a> {
+    pub fn new(
+        dictionary: &'a Dictionary,
+        template: &'a str,
+        spare_letters: &'a mut [char],
+    ) -> Iter<'a> {
+        let n_gaps = template.chars().filter(|&c| c == '.').count();
+
+        Iter {
+            dictionary,
+            permuter: permute::Iter::new(spare_letters, n_gaps),
+            template,
+            result_buf: String::new(),
+        }
+    }
+
+    pub fn next(&mut self) -> Option<&str> {
+        while let Some(chosen_letters) = self.permuter.next() {
+            self.result_buf.clear();
+
+            let mut chosen_letters = chosen_letters.iter();
+
+            for ch in self.template.chars() {
+                if ch == '.' {
+                    self.result_buf.push(*chosen_letters.next().unwrap());
+                } else {
+                    self.result_buf.push(ch);
+                }
+            }
+
+            if self.dictionary.contains(&self.result_buf) {
+                return Some(&self.result_buf);
+            }
+        }
+
+        None
+    }
+}
