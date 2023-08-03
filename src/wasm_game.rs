1ad156eb07dbfbd74da1b3b47a8b3d581de5ea5e
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-08-03 12:18:07 +0200

    wasm_game: Handle dragging the letters

--- src/wasm_game.rs
+++ src/wasm_game.rs
@@ -219,12 +219,22 @@ impl Loader {
     }
 }
 
+struct Drag {
+    position: usize,
+    start_x: i32,
+    start_y: i32,
+}
+
 struct Vaflo {
     context: Context,
+    mousedown_closure: Option<Closure::<dyn Fn(JsValue)>>,
+    mouseup_closure: Option<Closure::<dyn Fn(JsValue)>>,
+    mousemove_closure: Option<Closure::<dyn Fn(JsValue)>>,
     puzzles: Vec<Grid>,
     game_grid: web_sys::HtmlElement,
     letters: Vec<web_sys::HtmlElement>,
     grid: Grid,
+    drag: Option<Drag>,
 }
 
 impl Vaflo {
@@ -237,20 +247,74 @@ impl Vaflo {
 
         let grid = puzzles[0].clone();
 
-        let mut vaflo = Vaflo {
+        let mut vaflo = Box::new(Vaflo {
             context,
+            mousedown_closure: None,
+            mouseup_closure: None,
+            mousemove_closure: None,
             puzzles,
             game_grid,
             letters: Vec::with_capacity(WORD_LENGTH * WORD_LENGTH),
             grid,
-        };
+            drag: None,
+        });
 
+        vaflo.create_closures();
         vaflo.create_letters()?;
         vaflo.update_square_letters();
         vaflo.update_square_states();
         vaflo.show_grid();
 
-        Ok(Box::new(vaflo))
+        Ok(vaflo)
+    }
+
+    fn create_closures(&mut self) {
+        let vaflo_pointer = self as *mut Vaflo;
+
+        let mousedown_closure = Closure::<dyn Fn(JsValue)>::new(
+            move |event: JsValue| {
+                let vaflo = unsafe { &mut *vaflo_pointer };
+                let event: web_sys::MouseEvent = event.dyn_into().unwrap();
+                vaflo.handle_mousedown_event(event);
+            }
+        );
+
+        let _ = self.context.document.add_event_listener_with_callback(
+            "mousedown",
+            mousedown_closure.as_ref().unchecked_ref(),
+        );
+
+        self.mousedown_closure = Some(mousedown_closure);
+
+        let mouseup_closure = Closure::<dyn Fn(JsValue)>::new(
+            move |event: JsValue| {
+                let vaflo = unsafe { &mut *vaflo_pointer };
+                let event: web_sys::MouseEvent = event.dyn_into().unwrap();
+                vaflo.handle_mouseup_event(event);
+            }
+        );
+
+        let _ = self.context.document.add_event_listener_with_callback(
+            "mouseup",
+            mouseup_closure.as_ref().unchecked_ref(),
+        );
+
+        self.mouseup_closure = Some(mouseup_closure);
+
+        let mousemove_closure = Closure::<dyn Fn(JsValue)>::new(
+            move |event: JsValue| {
+                let vaflo = unsafe { &mut *vaflo_pointer };
+                let event: web_sys::MouseEvent = event.dyn_into().unwrap();
+                vaflo.handle_mousemove_event(event);
+            }
+        );
+
+        let _ = self.context.document.add_event_listener_with_callback(
+            "mousemove",
+            mousemove_closure.as_ref().unchecked_ref(),
+        );
+
+        self.mousemove_closure = Some(mousemove_closure);
     }
 
     fn create_letters(&mut self) -> Result<(), String> {
@@ -298,6 +362,88 @@ impl Vaflo {
         let _ = self.game_grid.style().set_property("display", "grid");
     }
 
+    fn position_for_event(&self, event: &web_sys::Event) -> Option<usize> {
+        let Some(target) = event.target()
+        else {
+            return None;
+        };
+
+        let Ok(element) = target.dyn_into::<web_sys::HtmlElement>()
+        else {
+            return None;
+        };
+
+        for (position, letter_element) in self.letters.iter().enumerate() {
+            if grid::is_gap_position(position) {
+                continue;
+            }
+
+            if letter_element == &element {
+                return Some(position);
+            }
+        }
+
+        None
+    }
+
+    fn update_drag_position(&self, event: &web_sys::MouseEvent) {
+        let drag = self.drag.as_ref().unwrap();
+
+        let left = event.client_x() - drag.start_x;
+        let top = event.client_y() - drag.start_y;
+        let style = self.letters[drag.position].style();
+
+        let _ = style.set_property("left", &format!("{}px", left));
+        let _ = style.set_property("top", &format!("{}px", top));
+    }
+
+    fn handle_mousedown_event(&mut self, event: web_sys::MouseEvent) {
+        if self.drag.is_some() {
+            return;
+        }
+
+        let Some(position) = self.position_for_event(&event)
+        else {
+            return;
+        };
+
+        event.prevent_default();
+
+        match self.grid.puzzle.squares[position].state {
+            PuzzleSquareState::Correct => return,
+            PuzzleSquareState::Wrong => (),
+            PuzzleSquareState::WrongPosition => (),
+        }
+
+        self.set_square_class(position, true);
+
+        self.drag = Some(Drag {
+            position,
+            start_x: event.client_x(),
+            start_y: event.client_y(),
+        });
+
+        self.update_drag_position(&event);
+    }
+
+    fn handle_mouseup_event(&mut self, event: web_sys::MouseEvent) {
+        let Some(drag) = self.drag.take()
+        else {
+            return;
+        };
+
+        event.prevent_default();
+
+        self.set_square_class(drag.position, false);
+    }
+
+    fn handle_mousemove_event(&mut self, event: web_sys::MouseEvent) {
+        if self.drag.is_some() {
+            event.prevent_default();
+            self.update_drag_position(&event);
+        }
+    }
+
     fn update_square_letter(&self, position: usize) {
         let element = &self.letters[position];
 
@@ -321,28 +467,34 @@ impl Vaflo {
         }
     }
 
+    fn set_square_class(&self, position: usize, dragging: bool) {
+        let element = &self.letters[position];
+
+        let square = &self.grid.puzzle.squares[position];
+
+        let class = match square.state {
+            PuzzleSquareState::Correct => "letter correct",
+            PuzzleSquareState::WrongPosition => "letter wrong-position",
+            PuzzleSquareState::Wrong => "letter wrong",
+        };
+
+        if dragging {
+            let _ = element.set_attribute(
+                "class",
+                &format!("{} dragging", class),
+            );
+        } else {
+            let _ = element.set_attribute("class", class);
+        }
+    }
+
     fn update_square_states(&self) {
-        for (position, square) in self
-            .grid
-            .puzzle
-            .squares
-            .iter()
-            .enumerate()
-        {
+        for position in 0..WORD_LENGTH * WORD_LENGTH {
             if grid::is_gap_position(position) {
                 continue;
             }
 
-            let element = &self.letters[position];
-
-            let _ = element.set_attribute(
-                "class",
-                match square.state {
-                    PuzzleSquareState::Correct => "letter correct",
-                    PuzzleSquareState::WrongPosition => "letter wrong-position",
-                    PuzzleSquareState::Wrong => "letter wrong",
-                }
-            );
+            self.set_square_class(position, false);
         }
     }
 }
