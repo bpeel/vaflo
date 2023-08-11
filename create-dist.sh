a0ad309781d50f8f1f4234123ae04bcf702c5622
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-08-11 11:55:34 +0200

    create-dist: Move the pkg files into a directory with the md5 hash
    That way the .js and the .wasm file can’t get out of sync if one is
    cached but not the other.

--- create-dist.sh
+++ create-dist.sh
@@ -10,8 +10,11 @@ files=(
     "puzzles.txt"
     "vaflo.js"
     "vaflo.css"
-    "pkg/vaflo_bg.wasm"
-    "pkg/vaflo.js"
+)
+
+pkg_files=(
+    "vaflo_bg.wasm"
+    "vaflo.js"
 )
 
 for x in "${files[@]}"; do
@@ -20,3 +23,14 @@ for x in "${files[@]}"; do
     mkdir -p "$dn"
     cp -v "$x" "$dn/$bn"
 done
+
+pkg_md5=$(cat "${pkg_files[@]/#/pkg\//}" | md5sum - | sed 's/ .*//')
+pkg_dir="dist/pkg-$pkg_md5"
+
+mkdir -p "$pkg_dir"
+
+for x in "${pkg_files[@]}"; do
+    cp -v "pkg/$x" "$pkg_dir/$x"
+done
+
+sed -i 's|\./pkg/vaflo\.js|./pkg-'"$pkg_md5"'/vaflo.js|' dist/vaflo.js
